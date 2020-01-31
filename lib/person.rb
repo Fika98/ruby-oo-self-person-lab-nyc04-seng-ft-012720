4158d9e59ff050fc348d95d6524663a1f15c1645
Fika98 <Afzalov98@gmail.com>
2020-01-31 00:19:16 -0500

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,84 @@
-# your code goes here
+class Person
+    attr_reader :name
+    attr_accessor :bank_account, :happiness, :hygiene
+    def initialize(name)
+        @name = name
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
+    end
+
+    def happiness=(happiness)
+        # puts "#{@happiness.to_s}  #{happiness} "
+        if happiness >= 10 
+            @happiness = 10
+        elsif happiness < 0 
+            @happiness = 0 
+        else
+            @happiness = happiness 
+            
+        end
+    end 
+    
+
+    def hygiene=(hygiene)
+        if hygiene >= 10 
+            @hygiene = 10
+        elsif hygiene < 0 
+            @hygiene = 0 
+        else
+            @hygiene = hygiene 
+            
+        end
+    end 
+
+    def happy?
+        happiness > 7 
+    end 
+
+    def clean?
+        hygiene > 7
+    end 
+
+    def get_paid(salary)
+        @bank_account += salary
+        "all about the benjamins"
+    end 
+
+    def take_bath
+        #puts @hygiene + 4
+        self.hygiene=(@hygiene + 4)
+        "♪ Rub-a-dub just relaxing in the tub ♫"
+    end 
+
+    def work_out 
+        # @hygiene -= 3
+        self.hygiene=(@hygiene -3)
+        self.happiness=(@happiness +2)
+        "♪ another one bites the dust ♫"
+        #@happiness += 2
+    end 
+
+    def call_friend(friend)
+        # puts self.inspect
+        self.happiness=(@happiness + 3)
+        friend.happiness=(friend.happiness + 3)
+        "Hi #{friend.name}! It's #{self.name}. How are you?"
+    end 
+
+    def start_conversation(convo,topic)
+        if topic == "politics"
+        convo.happiness=(@happiness - 2)
+        self.happiness=(@happiness - 2)
+        "blah blah partisan blah lobbyist" 
+        elsif topic == "weather"
+            convo.happiness=(@happiness + 1)
+            self.happiness=(@happiness + 1)
+            "blah blah sun blah rain"
+        else topic == "politics" || topic == "weather"
+            "blah blah blah blah blah"
+        end 
+    end 
+
+
+end 
